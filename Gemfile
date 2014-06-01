3dbd889fc01ea9e50f233042aaa2ea4a0c81d062
Tim Kamanin <tim@timonweb.com>
2014-06-01 15:42:34 +0300

    Removed tests from the old project

--- Gemfile
+++
@@ -1,4 +0,0 @@
-source 'https://rubygems.org'
-
-gem 'watchr', '~> 0.7'
-gem 'travis-lint'
--- autotest.rb
+++
@@ -1,4 +0,0 @@
-watch('.*\.(py|html|txt)') do |match_data_object|
-  system('python setup.py install --force')
-  system('cd test_project && python manage.py test --noinput --with-sneazr')
-end
